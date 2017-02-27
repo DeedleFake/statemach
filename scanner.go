0c618ddf018bd2332ee799384c7542bd5a31984e
DeedleFake <deedlefake@users.noreply.github.com>
2017-02-27 17:09:35 -0500

    Godoc.

--- scanner.go
+++ scanner.go
@@ -5,6 +5,8 @@ import (
 	"unicode"
 )
 
+// A Scanner is a StateMachine which implements a simple, whitespace
+// seperated tokenizer.
 type Scanner struct {
 	mapper func([]byte) (Token, error)
 
@@ -12,12 +14,23 @@ type Scanner struct {
 	tok Token
 }
 
+// NewScanner returns a new scanner which uses the given mapper
+// function to convert byte slices to tokens. It works like this:
+//
+//     1. The user calls StateMachine.Scan() for the StateMachine that's running the Scanner.
+//     2. The Scanner skips over whitespace runes.
+//     3. The Scanner begins reading runes one at a time, calling mapper on the slowly growing byte slice that reading the runes results in.
+//     4. If mapper returns an error, the StateMachine's error is set to that value and scanning stops.
+//     5. If mapper returns a non-nil token, then the scanner's current token is set to that value and scanning stops.
 func NewScanner(mapper func([]byte) (Token, error)) *Scanner {
 	return &Scanner{
 		mapper: mapper,
 	}
 }
 
+// Config returns a config that can be used when creating a
+// StateMachine that will cause the newly initialized StateMachine to
+// run the Scanner.
 func (s *Scanner) Config() Config {
 	return Config{
 		Initial: s.initial,
@@ -51,8 +64,11 @@ func (s *Scanner) match(c Controller, r rune) StateFunc {
 	return s.match
 }
 
+// Tok returns the latest token read by the Scanner.
 func (s *Scanner) Tok() Token {
 	return s.tok
 }
 
+// A Token represents a value yielded by a Scanner. It is defined as
+// its own type purely for self-documentation purposes.
 type Token interface{}
--- statemach.go
+++ statemach.go
@@ -1,3 +1,5 @@
+// Package statemach defines a number of helpers for creating state
+// machine implementations.
 package statemach
 
 import (
@@ -7,6 +9,11 @@ import (
 	"reflect"
 )
 
+// A StateMachine is a simplistic implementation of a DFA-style
+// automaton. Since this is Go, however, it isn't limited by the
+// traditional DFA definition. For example, a StateMachine, depending
+// on implementation, can have memory, and the states themselves can
+// have side effects.
 type StateMachine struct {
 	r    io.RuneReader
 	rbuf []rune
@@ -15,6 +22,8 @@ type StateMachine struct {
 	mach Config
 }
 
+// New returns a new StateMachine which reads from the given source
+// and uses the given config.
 func New(r io.Reader, mach Config) *StateMachine {
 	return &StateMachine{
 		r:    bufio.NewReader(r),
@@ -37,6 +46,15 @@ func (sm *StateMachine) unread(r rune) {
 	sm.rbuf = append(sm.rbuf, r)
 }
 
+// Run runs a single iteration of a StateMachine. It starts at the
+// initial state specified in the machines configuration and
+// repeatedly calls state functions until one of them returns nil or
+// an error occurs.
+//
+// Note that io.EOF is a special-case error. See the documentation for
+// Config for more information.
+//
+// Run returns true if more iterations can be run. In other words, if the machine was stopped because a state told it too, rather than because of an error.
 func (sm *StateMachine) Run() bool {
 	if sm.err != nil {
 		return false
@@ -63,6 +81,9 @@ func (sm *StateMachine) Run() bool {
 				sm.err = err
 				r = n
 
+			case nil:
+				return false
+
 			default:
 				panic(fmt.Errorf("Unexpected EOF handler type: %q", reflect.TypeOf(n)))
 			}
@@ -71,24 +92,69 @@ func (sm *StateMachine) Run() bool {
 		state = state(&controller{sm}, r)
 	}
 
-	return true
+	return sm.err == nil
 }
 
+// Err returns the error that stopped the StateMachine. If that error was io.EOF, Err returns nil.
 func (sm *StateMachine) Err() error {
+	if sm.err == io.EOF {
+		return nil
+	}
+
 	return sm.err
 }
 
+// A Config defines a StateMachine's behavior.
 type Config struct {
+	// Initial specifies the state that the StateMachine should start in
+	// whenever Run is called.
 	Initial StateFunc
-	EOF     interface{}
+
+	// EOF defines how the StateMachine should handle an io.EOF error
+	// while reading from the underlying input. When an io.EOF is
+	// encountered, the behavior of the machine is dependent on the
+	// type of EOF:
+	//
+	// If EOF is of type EOFFunc, then that function is called. If
+	// that function returns a non-nil StateFunc, then that returned
+	// function becomes the next state. Note that this will likely
+	// call EOF again if no runes were unread. If the function
+	// returned is nil, then the StateMachine will stop immediately.
+	//
+	// If EOF is of type rune, then the current StateFunc of the
+	// StateMachine will be called with that rune as its argument. In
+	// this case, a StateFunc must eventually return a nil StateFunc
+	// in order to stop the machine.
+	//
+	// If EOF is nil, then the machine will stop immediately.
+	//
+	// All other types will panic.
+	EOF interface{}
 }
 
+// A StateFunc represents a state in the StateMachine. As the machine
+// reads runes from the underlying input, it calls states repeatedly.
+// After each state, the state returned by the StateFunc becomes the
+// next state. If a StateFunc returns nil, then the StateMachine
+// stops.
 type StateFunc func(Controller, rune) StateFunc
 
+// An EOFFunc is a pseudo-state that is called when a StateMachine
+// encouters an io.EOF. For more information, see the documentation
+// for Config.
 type EOFFunc func(Controller) StateFunc
 
+// Controller gives a limited, specialized interface that allows some
+// control of parts of a StateMachine from inside state functions.
 type Controller interface {
+	// Unread pushes a rune onto a stack, causing it to be the next rune
+	// read by the StateMachine instead of whatever would have been read
+	// from the underlying input.
 	Unread(rune)
+
+	// SetErr sets the StateMachine's error value. This does not stop
+	// the current StateMachine run, but it will prevent future runs,
+	// and it will cause the Err method to return the value given to it.
 	SetErr(error)
 }
 
